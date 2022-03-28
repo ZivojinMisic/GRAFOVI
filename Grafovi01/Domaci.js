b799e88c83a7fa8f3877843af8aede87db5aa9de
ZivojinMisic <97547019+ZivojinMisic@users.noreply.github.com>
2022-03-28 07:38:53 +0200

    Add files via upload

---
+++ Grafovi01/Domaci.js
@@ -0,0 +1,68 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.adjacents = [];
+    }
+  
+    addAdjacent(node) {
+      this.adjacents.push(node);
+    }
+  
+    removeAdjacent(node) {
+      const index = this.adjacents.indexOf(node);
+      if(index > -1) {
+        this.adjacents.splice(index, 1);
+        return node;
+      }
+    }
+  
+    getAdjacents() {
+      return this.adjacents;
+    }
+  
+    isAdjacent(node) {
+      return this.adjacents.indexOf(node) > -1;
+    }
+  }
+class Graph {
+    constructor(edgeDirection = Graph.DIRECTED) {
+      this.nodes = new Map();
+      this.edgeDirection = edgeDirection;
+    }
+    addVertex(value) {
+        if(this.nodes.has(value)) {
+          return this.nodes.get(value);
+        } else {
+          const vertex = new Node(value);
+          this.nodes.set(value, vertex);
+          return vertex;
+        }
+      }
+    removeVertex(value) {
+        const current = this.nodes.get(value);
+        if(current) {
+          for (const node of this.nodes.values()) {
+            node.removeAdjacent(current);
+          }
+        }
+        return this.nodes.delete(value);
+    }
+    removeEdge(source, destination) {
+        const sourceNode = this.nodes.get(source);
+        const destinationNode = this.nodes.get(destination);
+      
+        if(sourceNode && destinationNode) {
+          sourceNode.removeAdjacent(destinationNode);
+      
+          if(this.edgeDirection === Graph.UNDIRECTED) {
+            destinationNode.removeAdjacent(sourceNode);
+          }
+        }
+      
+        return [sourceNode, destinationNode];
+    }
+}
+  
+  Graph.UNDIRECTED = Symbol('directed graph');
+  Graph.DIRECTED = Symbol('undirected graph');
+  
